5b38ec5b880b3aff114d93ae9f8a654841ecaa4f
Cesario Nivar <ing.cesarionivar@gmail.com>
2025-03-11 00:56:00 -0400

    Initial Commit

---
+++ TPGBucket/TPGBucketApp.swift
@@ -0,0 +1,17 @@
+//
+//  TPGBucketApp.swift
+//  TPGBucket
+//
+//  Created by Cesario Nivar on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TPGBucketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
